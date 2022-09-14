bc87d76b63f7ca819ff8489f17737395b10d461c
Pauline299 <pauline.ledoux29@hotmail.fr>
2022-09-14 14:56:05 +0200

    savecoursesrouter

--- src/routers/courses.router.js
+++ src/routers/courses.router.js
@@ -2,12 +2,13 @@ import express from "express";
 import Course from "../models/course.model.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import auth from "../middleware/auth.js"
 
 dotenv.config();
 
 const coursesRouter = express.Router();
 
-coursesRouter.post("/", async (req, res) => {
+coursesRouter.post("/", auth, async (req, res) => {
 	const course = new Course(req.body);
 	return console.log(course);
 	//course.author = req.user._id;
