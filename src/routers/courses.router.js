1bc36c392941c55c90d4917439035ded164fedcd
SophieC Adou <sophieadou@hotmail.fr>
2022-09-14 15:37:48 +0200

    modif course

--- src/routers/courses.router.js
+++ src/routers/courses.router.js
@@ -8,14 +8,36 @@ dotenv.config();
 
 const coursesRouter = express.Router();
 
+//afficher tous les cours
 coursesRouter.get("/", async (_, res) => {
 	const courses = await Course.find();
 	return res.send(courses);
 });
 
+//créer un cours
 coursesRouter.post("/", auth, async (req, res) => {
 	const course = new Course(req.body);
 	course.author = req.user._id;
+	await course.save();
+	return res.send(course);
+});
+
+// /!\ Vérifier si l'authentifié est l'auteur du cours
+coursesRouter.patch("/:id", auth, async (req, res) => {
+	if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+		return res.sendStatus(404);
+	}
+	const course = await Course.findById(req.params.id);
+	if (!course) {
+		return res
+			.status(404)
+			.send(`Le cours avec l'id ${req.params.id} n'existe pas`);
+	}
+
+	for (let attribut in req.body) {
+		course[attribut] = req.body[attribut];
+	}
+
 	await course.save();
 	return res.send(course);
 });
