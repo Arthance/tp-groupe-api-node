4cfb95cb727c43f23fe4382659296e7bdfbd3a32
SophieC Adou <sophieadou@hotmail.fr>
2022-09-14 11:13:59 +0200

    modif url et import

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
-import contactsRouter from "./contacts.router.js";
 import usersRouter from "./users.router.js";
+import coursesRouter from "./courses.router.js";
+import videosRouter from "./videos.router.js";
 
 const PORT = process.env.PORT || 3000;
 
-export default function bootstrap(app) {
+export default function loader(app) {
 	app.use(express.json());
 	app.use(cors());
-	app.use("/api/contacts", contactsRouter);
-	app.use("/api/users", usersRouter);
+	app.use("/jojolearning/users", usersRouter);
+	app.use("/jojolearning/videos", videosRouter);
+	app.use("/jojolearning/courses", coursesRouter);
 	app.listen(PORT, () => {
-		console.log(`Le serveur a demarré sur le port ${PORT}🚀🚀`);
+		console.log(`Le serveur a demarré sur le port ${PORT}`);
 	});
 }
