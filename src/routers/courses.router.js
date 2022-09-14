f220b2ff9b8d8b46dcd318ece65236e0114a2e93
SophieC Adou <sophieadou@hotmail.fr>
2022-09-14 14:48:59 +0200

    post working with middleware auth

--- src/routers/courses.router.js
+++ src/routers/courses.router.js
@@ -2,17 +2,17 @@ import express from "express";
 import Course from "../models/course.model.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import auth from "../middleware/auth.js";
 
 dotenv.config();
 
 const coursesRouter = express.Router();
 
-coursesRouter.post("/", async (req, res) => {
+coursesRouter.post("/", auth, async (req, res) => {
 	const course = new Course(req.body);
-	return console.log(course);
-	//course.author = req.user._id;
-	//await course.save();
-	//return res.send(course);
+	course.author = req.user._id;
+	await course.save();
+	return res.send(course);
 });
 
 export default coursesRouter;
