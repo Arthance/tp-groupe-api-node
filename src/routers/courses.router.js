720297659d7142b4cf68598a6dbae43319e4a7ec
Arthur Herminie <arthur.herminie@gmail.com>
2022-09-14 16:04:20 +0200

    added the isInstructor middleware to post course route

--- src/routers/courses.router.js
+++ src/routers/courses.router.js
@@ -3,16 +3,17 @@ import Course from "../models/course.model.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import auth from "../middleware/auth.js";
+import isInstructor from "../middleware/is.instructor.js";
 
 dotenv.config();
 
 const coursesRouter = express.Router();
 
-coursesRouter.post("/", auth, async (req, res) => {
-	const course = new Course(req.body);
-	course.author = req.user._id;
-	await course.save();
-	return res.send(course);
+coursesRouter.post("/", [auth, isInstructor], async (req, res) => {
+  const course = new Course(req.body);
+  course.author = req.user._id;
+  await course.save();
+  return res.send(course);
 });
 
 export default coursesRouter;
